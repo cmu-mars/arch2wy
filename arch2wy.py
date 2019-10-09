0a6d6634c32dd0e6201bf2bb57e17bd413c23271
Ian Voysey <iev@cs.cmu.edu>
2019-10-09 17:00:39 -0400

    names of components

--- arch2wy.py
+++ arch2wy.py
@@ -1,9 +1,23 @@
 #!/usr/local/bin/python
 
 import yaml
+import sys
 
-with open("architecture.yaml", 'r') as stream:
+def rename(x):
+    return (x.replace('/','').replace('_',' ')).title().replace(' ','')
+
+if not len(sys.argv) == 2:
+    sys.exit("please provide exactly one command line argument")
+
+with open(sys.argv[1], 'r') as stream:
     try:
-        print(yaml.safe_load(stream))
+        arch = yaml.safe_load(stream)
     except yaml.YAMLError as exc:
         print(exc)
+        sys.exit("failed to read yaml file")
+
+for node in arch:
+    print "component " + rename(node["fullname"])
+    print "\t val name: String"
+    print
+#    for k in node.keys():
